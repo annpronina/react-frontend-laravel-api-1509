37a735b9ec433a37bbe5030fd51df63b2282e37e
Your Name <you@example.com>
2025-09-15 15:32:49 +0300

    first commit

---
+++ src/assets/Pages/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <>
+        <header>
+            <nav>
+                <Link to="/" className="nav-link">Home</Link>
+            </nav>
+        </header>
+        </>
+    );
+}
